fcc9342ad276834f537af38616d7fe0964b703d0
Abdelaziz <abdelazizMalfy@gmail.com>
2019-09-03 18:03:56 +0200

    login logic added

--- server/server.js
+++ server/server.js
@@ -24,6 +24,8 @@ const { User } = require('./models/user');
 //            USERS
 //=============================
 
+//Register 
+
 app.post('/api/users/register', (req,res) =>{
     const user = new User(req.body)
 
@@ -35,6 +37,34 @@ app.post('/api/users/register', (req,res) =>{
         })
     })
 })
+
+// Login
+
+app.post('/api/users/login', (req,res) =>{
+    User.findOne({'email':req.body.email},(err,user) =>{
+        if(!user) return res.json({loginSuccess: false, message:'Email not found'})
+
+        // User found --> compare password
+
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(!isMatch) return res.json({loginSuccess:false,message:'password is incorrect' })
+        
+            // passwords matched ---> generate token
+
+            user.generateToken((err,user)=>{
+                if(err) return res.status(400).send(err)
+
+                // Token is generated --> save it in cookies
+
+                res.cookie('w_auth',user.token).status(200).json({loginSuccess:true})
+            })
+
+        })
+    })
+})
+
+
+
 
 
 
